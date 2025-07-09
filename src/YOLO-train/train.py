892cfa553fdb303cfa9c23c6991dcd9e4373d35c
atlas <atlaso4ek@gmail.com>
2025-07-09 15:42:02 +0300

    YOLO graffiti detection v0.001

--- src/YOLO-train/train.py
+++ src/YOLO-train/train.py
@@ -6,5 +6,3 @@ if __name__ == '__main__':
     results = model.train(data='google_colab_config.yaml', epochs=100000, optimizer='Adam',
                           lr0=0.0001, device=0, patience=7, dropout=0.4,
                           save=True, save_period=5)
-
-    print('a')
