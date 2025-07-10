88ae24c7f309289d613d5313095dcc441221572a
atlas <atlaso4ek@gmail.com>
2025-07-10 12:03:16 +0300

    YOLO graffiti detection v0.002a

--- src/API/API.py
+++ src/API/API.py
@@ -1,25 +1,32 @@
 import io
-
 from flask import Flask
 from ultralytics import YOLO
 import base64
 from PIL import Image
+import os
+
 
 app = Flask(__name__)
-model = YOLO('YOLO.pt')
+model = YOLO('src/API/YOLO.pt')
+
+@app.route('/')
+def hello_world():
+  return 'Hello, World!'
+
+@app.route('/abduz/<name>')
+def he_world(name):
+  return f'Hello, World! {name}'
 
 @app.route('/get_boxes/<img>')
-def get_boxes(img=None):
-    if img==None:
-        return 'No Image', 404
-    results = model.predict([Image.open(io.BytesIO(base64.b64decode(img)))])
+def get_boxes(img):
+    #return io.BytesIO(base64.urlsafe_b64decode(img))
+    results = model.predict([Image.open(io.BytesIO(base64.urlsafe_b64decode(img)))])
     response = {}
     for result in results:
         boxes = result.boxes  # Boxes object for bounding box outputs
         for i in range(len(boxes.cls)):
             response.update({int(boxes.cls[i]):list(boxes.xywh[i])})
 
-    return response, 200
+    return response
 
-if __name__ == 'main':
-  app.run(debug=True)
+app.run(debug=True)
