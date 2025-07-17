b0346982424cdf022ece31632884d76c2beae518
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 <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>
2025-07-17 17:01:45 +0300

    AAAAAAА

--- llm_tree.py
+++ llm_tree.py
@@ -18,23 +18,27 @@ def correct_ask_model(model: LLM, history: History, correct_answer: str):
 def get_answer(image_path):
     history_with_image = History([Message('user', None, Message.encode_image(image_path))])
 
+    # есть ли вывеска с кронштейном на фото
     qwen_kronchtein = get_learning_llm_from_data(QwenLLM(QWEN_API_KEY), "data/learning/learning_data_kronchtein.json")    
     if not correct_ask_model(qwen_kronchtein, history_with_image, "YES"):
         return 1
     
+    # находится ли вывеска с кронштейном на высоте  >2.5метров от земли 
     qwen_height = get_learning_llm_from_data(QwenLLM(QWEN_API_KEY), "data/learning/learning_data_height.json")    
     if not correct_ask_model(qwen_height, history_with_image, "да"):
         return 0
-
+    
+    # содержит ли вывеска с кронштейном лишнюю информацию
     qwen_unecc_info = get_learning_llm_from_data(QwenLLM(QWEN_API_KEY), "data/learning/learning_data_no_extra_info.json")
     if not correct_ask_model(qwen_unecc_info, history_with_image, "нет"):
         return 0
     
-    # есть ли вывеска на фото
+    # есть ли вывеска без кронштейна на фото
     qwen_board = get_learning_llm_from_data(QwenLLM(QWEN_API_KEY), "data/learning/learning_data_board.json")
     if not correct_ask_model(qwen_board, history_with_image, "YES"):
         return 1 # если вывески нет - значит все ок
     
+    # вывеска без кронштейна и вывеска с кронштейном находятся ли на одной горизонтальной линии
     qwen_similar_line = get_learning_llm_from_data(QwenLLM(QWEN_API_KEY), "data/learning/learning_data_axis.json")
     if not correct_ask_model(qwen_similar_line, history_with_image, "да"):
         return 0
